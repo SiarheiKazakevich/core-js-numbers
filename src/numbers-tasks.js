4cc06047e9dea8b5430befff1cc29baa35f2c299
SiarheiKazakevich <dianrom@tut.by>
2023-12-04 16:15:47 +0300

    feat: checked finished toNumber task

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -234,8 +234,9 @@ function isPrime(n) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  const convertedValue = Number(value);
+  return Number.isNaN(convertedValue) ? def : convertedValue;
 }
 
 /**
